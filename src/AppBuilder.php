8ba6cbbed31830ddc0e36e2d7f89490421f7b409
Matthes <matthes@leuffen.de>
2021-02-13 17:46:12 +0100

    apploader

--- src/AppBuilder.php
+++ src/AppBuilder.php
@@ -7,4 +7,83 @@ namespace Brace\Core;
 class AppBuilder
 {
 
+    private static $appRoot = __DIR__ . "/../../../../app/";
+
+    private static $apps = [];
+
+    private static $appFilesAreLoaded = false;
+
+    public static function SetAppRoot(string $appRoot = __DIR__ . "/../../../../app/") : void
+    {
+        self::$appRoot = $appRoot;
+    }
+
+
+    /**
+     * Define a Application
+     *
+     * <example>
+     * AppBuilder::extend(function(BraceApp $app) {
+     *      $app->addModule(...)
+     * });
+     * </example>
+     *
+     * @param callable $loaderFn
+     * @param string $alias
+     */
+    public static function extend(callable $loaderFn, string $alias = "Default") : void
+    {
+        if ( ! isset (self::$apps[$alias]))
+            self::$apps[$alias] = [];
+        self::$apps[$alias][] = $loaderFn;
+    }
+
+
+    private static function requireFilesFromAppDir(string $appRoot) : void
+    {
+        if (self::$appFilesAreLoaded) {
+            return; // Files are already loaded
+        }
+        self::$appFilesAreLoaded = true;
+
+        if ( ! is_dir($appRoot))
+            throw new \InvalidArgumentException("App directory is not a directory: '$appRoot'");
+        $dir = opendir($appRoot);
+        if ($dir === false)
+            throw new \InvalidArgumentException("Cannot open app root directory: '$appRoot'");
+
+        $loaderFiles = [];
+        while ( false !== ($file = readdir($dir))) {
+            if ( ! preg_match("/^([0-9]{2,2})_[a-z0-9.-_]+\.php$/i", $file, $mathces)) {
+                continue;
+            }
+            $loaderFiles[] = $file;
+        }
+        asort($loaderFiles);
+        foreach ($loaderFiles as $loaderFile) {
+            require $appRoot . "/" . $loaderFile;
+        }
+    }
+
+    /**
+     * Load a application. Will require all php files from app folder, run
+     * the init scripts and return the App.
+     *
+     * @param string $alias
+     * @return BraceApp
+     */
+    public static function loadApp(string $alias = "Default") : BraceApp
+    {
+        self::requireFilesFromAppDir(self::$appRoot);
+
+        if ( ! isset (self::$apps[$alias]))
+            throw new \InvalidArgumentException("App '$alias' is not defined.");
+
+        $app = new BraceApp();
+        foreach (self::$apps[$alias] as $loaderFn) {
+            $loaderFn($app);
+        }
+        return $app;
+    }
+
 }
