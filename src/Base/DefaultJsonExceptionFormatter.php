8c7ec22c9c6eca2e895e8bfffda3720ebb5c8de7
Matthes <matthes@leuffen.de>
2021-02-16 22:46:23 +0100

    formatter changed

--- src/Base/DefaultJsonExceptionFormatter.php
+++ src/Base/DefaultJsonExceptionFormatter.php
@@ -12,16 +12,39 @@ class DefaultJsonExceptionFormatter implements ExceptionFormatterInterface
 {
 
     public function __construct(
-        private BraceApp $app
+        private BraceApp $app,
+        private bool $showDebugInfo = true
     ) {}
 
 
+
+
+    private function formatException(\Exception $e) : array
+    {
+        $base = [
+            "class" => get_class($e),
+            "message" => $e->getMessage(),
+        ];
+        if ($this->showDebugInfo) {
+            $base["file"] = $e->getFile() . " [Line:" . $e->getLine() . "]";
+        }
+        return $base;
+    }
+
     public function format(\Exception $e): ResponseInterface
     {
+        $errors = [$this->formatException($e)];
+        $prev = $e;
+        while (($prev = $prev->getPrevious()) !== null) {
+            $errors[] = $this->formatException($prev);
+        }
+
         $data = [
-            "success" => false,
-            "status" => "Exception",
-            "e_msg" => $e->getMessage()
+            "error" => [
+                "errors" => $errors,
+                "code" => $e->getCode(),
+                "message" => $e->getMessage()
+            ]
         ];
 
         $response = $this->app->responseFactory->createResponseWithBody(json_encode($data, JSON_PRESERVE_ZERO_FRACTION|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE|JSON_INVALID_UTF8_SUBSTITUTE), 500);
