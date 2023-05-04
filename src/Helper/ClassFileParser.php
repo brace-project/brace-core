3d64fbb0c8507f72f820cebf86d4ccba832b7237
Matthias Leuffen <matthes@leuffen.de>
2023-05-04 23:39:23 +0200

    added helper to load class names from file

---
+++ src/Helper/ClassFileParser.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Brace\Core\Helper;
+
+class ClassFileParser
+{
+
+    public static function extractPhpClasses(string $file)
+    {
+        $classes   = [];
+        $namespace = '';
+        $tokens    = \PhpToken::tokenize(file_get_contents($file));
+
+        for ($i = 0; $i < count($tokens); $i++) {
+            if ($tokens[$i]->getTokenName() === 'T_NAMESPACE') {
+                for ($j = $i + 1; $j < count($tokens); $j++) {
+                    if ($tokens[$j]->getTokenName() === 'T_NAME_QUALIFIED') {
+                        $namespace = $tokens[$j]->text;
+                        break;
+                    }
+                }
+            }
+
+            if ($tokens[$i]->getTokenName() === 'T_CLASS') {
+                for ($j = $i + 1; $j < count($tokens); $j++) {
+                    if ($tokens[$j]->getTokenName() === 'T_WHITESPACE') {
+                        continue;
+                    }
+
+                    if ($tokens[$j]->getTokenName() === 'T_STRING') {
+                        $classes[] = $namespace . '\\' . $tokens[$j]->text;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Contains all FQCNs found in a file.
+        return $classes;
+    }
+
+}
